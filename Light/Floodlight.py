7e5ab35f755f4443c41de305bc3466c8808f32d9
Raul Galvez <raul.galvez@duke.edu>
2024-12-06 12:58:39 -0500

    Update Floodlight.py

--- Light/Floodlight.py
+++ Light/Floodlight.py
@@ -8,16 +8,13 @@ RELAY_PIN = 27  # GPIO pin for the relay
 # Configure GPIO
 chip = gpiod.Chip(CHIP)  # Access the GPIO chip
 line = chip.get_line(RELAY_PIN)  # Get the specific GPIO line (pin)
-line.request(consumer="relay_control", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[1])  # Default to HIGH (relay OFF)
+line.request(consumer="relay_control", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[1])
 
 try:
     start_time = time.time()  # Record the start time
     print("Floodlight is on")
-    while time.time() - start_time < 5:  # Run the loop for 15 seconds
-        # Turn the relay on (active LOW)
+    while time.time() - start_time < 5:  # Run the loop for 5 seconds
         line.set_value(1)  # Activate relay
-    
-    line.set_value(0)
 
 except KeyboardInterrupt:
     print("Script stopped by the user")
